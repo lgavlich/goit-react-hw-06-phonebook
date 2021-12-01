49ff3534122ac886ffdbfeedc335095a9afa93f6
lgavlich <lgavlich@gmail.com>
2021-12-01 11:49:36 +0200

    store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-//import store from "./redux/store";
-//import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,50 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import contactsReducer from "./contacts-reducer";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 //import { combineReducers } from "redux";
 //import { composeWithDevTools } from "redux-devtools-extension";
 
-const middlware = [...getDefaultMiddleware(), logger];
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-  middlware,
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
+
+const rootReducer = combineReducers({
+  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware,
   devTools: process.env.NODE_ENV === "development",
 });
 
-//const rootReducer = combineReducers({
-//  contacts: contactsReducer,
-//});
+const persistor = persistStore(store);
 
-//const store = createStore(rootReducer, composeWithDevTools());
-
-//export default store;
+export default { store, persistor };
