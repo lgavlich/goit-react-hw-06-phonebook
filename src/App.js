27883af2d015fdef9888e7986327f745b89d240c
lgavlich <lgavlich@gmail.com>
2021-12-01 11:33:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Container from "./components/Container/Container";
 import Input from "./components/ContactList/ContactList";
-import Filter from "./components/Filter/Filter";
 import PhonebookList from "./components/PhonebookList/PhonebookList";
 import useLocalStorage from "./hooks/localStorage";
+//import { useState } from "react";
+//import Filter from "./components/Filter/Filter";
 
 export default function App() {
   const [contacts, setContacts] = useLocalStorage("contacts", []);
-  const [filter, setFilter] = useState("");
+  //const [filter, setFilter] = useState("");
   console.log(contacts);
 
   const formSubmit = (data) => {
@@ -28,32 +28,30 @@ export default function App() {
     setContacts((prevState) => [...prevState, contactData]);
   };
 
-  const deleteList = (listId) => {
-    setContacts((state) => state.filter((contact) => contact.id !== listId));
-  };
-
-  const changeFilter = (event) => {
-    setFilter(event.currentTarget.value);
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
   return (
     <Container>
       <h1>Phonebook</h1>
       <Input onFormSubmit={formSubmit} />
-      <Filter value={filter} onChangeFilter={changeFilter} />
       <PhonebookList
-        contacts={getVisibleContacts()}
+        //contacts={getVisibleContacts()}
         title="Contacts"
-        onDeleteList={deleteList}
+        //onDeleteList={deleteList}
       />
     </Container>
   );
 }
+//const deleteList = (listId) => {
+//  setContacts((state) => state.filter((contact) => contact.id !== listId));
+//};
+
+//const changeFilter = (event) => {
+//  setFilter(event.currentTarget.value);
+//};
+
+//const getVisibleContacts = () => {
+//  const normalizedFilter = filter.toLowerCase();
+
+//  return contacts.filter((contact) =>
+//    contact.name.toLowerCase().includes(normalizedFilter)
+//  );
+//};
